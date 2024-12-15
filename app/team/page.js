0c2b50e016b5d9f6ccc1ea5e5a5c130eacdeef2b
gautamkmahato <gautamkmahato@gmail.com>
2024-12-15 22:39:30 +0530

    new changes

--- app/team/page.js
+++ app/team/page.js
@@ -30,20 +30,20 @@ export default function BioSection() {
           subhashishk2@gmail.com
         </p>
         <p className={styles.bio}>
-            Subhashish Kumar&lsquo; the founder of Sahaj Nyay&apos; is a graduate of the National Law Institute University, 
-            Bhopal&apos; one of India premier law schools. He is registered with the Bar Council of Delhi and the Bar 
+            Subhashish Kumar&sbquo; the founder of Sahaj Nyay&sbquo; is a graduate of the National Law Institute University&sbquo;
+            Bhopal&sbquo; one of India premier law schools. He is registered with the Bar Council of Delhi and the Bar 
             Council of India and an active member of the Delhi High Court Bar Association.
-            Having walked the path of both privilege and humility&apos; Subhashish possesses a unique perspective 
+            Having walked the path of both privilege and humility&sbquo; Subhashish possesses a unique perspective 
             on the Indian legal system. His experiences with individuals from diverse socio economic backgrounds 
             have shaped his holistic understanding of justice and advocacy. This dual insight enables him to craft 
-            practical&apos; empathetic&apos; and effective legal strategies tailored to each clients needs.
-            With a sharp legal acumen and a steadfast commitment to the principles of fairness and integrity&apos; 
-            Subhashish founded Sahaj Nyay to simplify law and empower justice. Under his leadership&apos;
-            the firm embodies a solution-oriented ethos&apos; striving to provide efficient and client-centric 
+            practical&sbquo; empathetic&sbquo; and effective legal strategies tailored to each clients needs.
+            With a sharp legal acumen and a steadfast commitment to the principles of fairness and integrity&sbquo; 
+            Subhashish founded Sahaj Nyay to simplify law and empower justice. Under his leadership&sbquo;
+            the firm embodies a solution-oriented ethos&sbquo; striving to provide efficient and client-centric 
             legal services while upholding the highest professional standards.
-            Subhashish approach combines innovation&apos; precision, and a deep-rooted belief in delivering 
-            results. He envisions a legal system that is transparent&apos; accessible&apos; and truly serves the people 
-            it represents. Simplifying Law&apos; Empowering Justice.
+            Subhashish approach combines innovation&sbquo; precision, and a deep-rooted belief in delivering 
+            results. He envisions a legal system that is transparent&sbquo; accessible&sbquo; and truly serves the people 
+            it represents. Simplifying Law&sbquo; Empowering Justice.
         </p>
       </div>
     </div>
