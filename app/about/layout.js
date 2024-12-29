ab8d60c0d117f5cb7b83e6f2af1805b4a8e9952e
gautamkmahato <gautamkmahato@gmail.com>
2024-12-29 11:41:25 +0530

    changed for the SEO

--- app/about/layout.js
+++
@@ -1,7 +0,0 @@
-
-export const metadata = {
-    title: "Sahaj Nyay About Page",
-    description: "At Sahaj Nyay, we are dedicated to providing personalized, high-quality legal services with a focus on integrity, diligence, and results. Let us help you secure the justice and peace of mind you deserve.",
-};
-
-
